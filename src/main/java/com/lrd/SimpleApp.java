38ecc96d803967c5ae699b27b5ae121a9561a278
Lorduu <30489004+Lorduu@users.noreply.github.com>
2017-11-01 08:47:10 +0800

    my-spark-test

---
+++ src/main/java/com/lrd/SimpleApp.java
@@ -0,0 +1,145 @@
+package com.lrd;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.*;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
+import scala.Tuple2;
+
+import java.util.*;
+
+public class SimpleApp {
+    public static void main(String[] args) {
+        SimpleApp app = new SimpleApp();
+        app.test1();
+    }
+
+    void test1(){
+        System.setProperty("hadoop.home.dir", "D:\\Software\\Program\\hadoop");
+        String logFile = "D:\\project\\pom.xml";
+        SparkSession spark = SparkSession.builder().appName("Simple Application").config("spark.master", "local[*]").getOrCreate();
+        Dataset<String> logData = spark.read().textFile(logFile).cache();
+
+
+        long numAs = logData.filter(s -> s.contains("a")).count();
+        long numBs = logData.filter(s -> s.contains("b")).count();
+
+        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
+        spark.stop();
+    }
+
+    void test2(){
+        System.setProperty("hadoop.home.dir", "D:\\Software\\Program\\hadoop");
+        SparkConf conf = new SparkConf().setAppName("lrd sparkApplication").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
+        JavaRDD<Integer> distData = sc.parallelize(data);
+        int dataSum = distData.reduce((a,b) -> a + b);
+
+        JavaRDD<String> lines = sc.textFile("src/main/resources/data.txt");
+
+
+        //JavaRDD<Integer> lineLengths = lines.map(new Function<String, Integer>() {
+        //    @Override
+        //    public Integer call(String s) throws Exception {
+        //        return s.length();
+        //    }
+        //});
+
+        JavaRDD<Integer> lineLengths = lines.map(s -> s.length());
+        int totalLength = lineLengths.reduce((a, b) -> a + b);
+        System.out.println("输出totalLength: " + totalLength + "\ndataSum:" + dataSum );
+
+    }
+
+    void streamTest1(){
+        //System.setProperty("hadoop.home.dir", "D:\\Software\\Program\\hadoop");
+
+        SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("NetworkWordCount");
+        JavaSparkContext jsc = new JavaSparkContext(conf);
+        JavaStreamingContext jssc = new JavaStreamingContext(jsc, Durations.seconds(1));
+        jsc.setLogLevel("WARN");
+        // Create a DStream that will connect to hostname:port, like localhost:9999
+        JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost", 9999);
+        // Split each line into words
+        JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(x.split(" ")).iterator());
+        // Count each word in each batch
+        JavaPairDStream<String, Integer> pairs = words.mapToPair(s -> new Tuple2<>(s, 1));
+        JavaPairDStream<String, Integer> wordCounts = pairs.reduceByKey((i1, i2) -> i1 + i2);
+
+        // Print the first ten elements of each RDD generated in this DStream to the console
+        wordCounts.print();
+
+        jssc.start();              // Start the computation
+
+        try {
+            jssc.awaitTermination();   // Wait for the computation to terminate
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        jssc.close();
+    }
+
+    void streamTest2(){
+        String brokers = "127.0.0.1:9092";
+        String topics = "test";
+        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("streaming word count");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        sc.setLogLevel("WARN");
+        JavaStreamingContext ssc = new JavaStreamingContext(sc, Durations.seconds(1));
+
+        Collection<String> topicsSet = new HashSet<>(Arrays.asList(topics.split(",")));
+        //kafka相关参数，必要！缺了会报错
+        Map<String, Object> kafkaParams = new HashMap<>(16);
+        kafkaParams.put("metadata.broker.list", brokers);
+        kafkaParams.put("bootstrap.servers", brokers);
+        kafkaParams.put("group.id", "group1");
+        kafkaParams.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        kafkaParams.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        kafkaParams.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        // Topic分区
+        Map<TopicPartition, Long> offsets = new HashMap<>(16);
+        offsets.put(new TopicPartition("test", 0), 2L);
+
+        // 通过KafkaUtils.createDirectStream(...)获得kafka数据，kafka相关参数由kafkaParams指定
+        JavaInputDStream<ConsumerRecord<Object, Object>> lines = KafkaUtils.createDirectStream(
+                ssc,
+                LocationStrategies.PreferConsistent(),
+                ConsumerStrategies.Subscribe(topicsSet, kafkaParams, offsets)
+        );
+
+        // 这里就跟之前的demo一样了，只是需要注意这边的lines里的参数本身是个ConsumerRecord对象
+        JavaPairDStream<String, Integer> counts =
+                lines.flatMap(x -> Arrays.asList(x.value().toString().split(" ")).iterator())
+                        .mapToPair(x -> new Tuple2<>(x, 1))
+                        .reduceByKey((x, y) -> x + y);
+        counts.print();
+
+//        // 可以打印所有信息，看下ConsumerRecord的结构
+//        lines.foreachRDD(rdd -> rdd.foreach(x -> {
+//            System.out.println(x);
+//        }));
+
+        ssc.start();
+        try {
+            ssc.awaitTermination();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ssc.close();
+    }
+
+}
